57858076d7ba5d8113b76f8d4c48f03c9cd4da50
Yaroslav <ynagin@splitdev.pro>
2020-06-23 17:03:06 +0300

    start project

---
+++ resources/assets/scripts/main.js
@@ -0,0 +1,36 @@
+// Polyfills if needed
+// require('intersection-observer');
+
+// import external dependencies
+import './util/polyfills'
+import 'jquery';
+
+// Import everything from autoload
+import './autoload/**/*'
+
+// import local dependencies
+import Router from './util/Router';
+import common from './routes/common';
+import Detection from './util/Detection';
+
+window._detector = new Detection({
+ detect: ['ie11'],
+});
+window._detector.init();
+
+// /** Populate Router instance with DOM routes */
+const routes = new Router({
+  // All pages
+  common,
+});
+
+// // Load Events
+jQuery(document).ready(() => routes.loadEvents());
+
+$('#show').on('click', function () {
+  $('.first').addClass('open');
+})
+
+$('#hide').on('click', function () {
+  $('.first').removeClass('open');
+})
---
+++ resources/assets/scripts/routes/common.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+export default {
+  init() {
+
+  },
+
+  // JavaScript to be fired on all pages, after page specific JS is fired
+  finalize() {
+  },
+};
